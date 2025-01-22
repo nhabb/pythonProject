6381150c7a88ec2d98bd9b46d45f12a71decb156
HaniMaanak <hmaanak@gmail.com>
2025-01-22 11:30:01 +0200

    adjusted stdGrades

--- projectpy/finalversion.py
+++ projectpy/finalversion.py
@@ -193,3 +193,34 @@ def createFile():
 
 if __name__ == "__main__" :
     main()
+
+def stdGrades( stdID ) :
+    files = []
+    with open( "courses.csv" ) as file :
+        reader = csv.reader( file )
+        for row in reader :
+            files.append( row[1] )
+
+    stdRecord = []
+    for name in files[1:] :
+        with open( name+".csv" ) as file :
+            reader = csv.reader( file )
+            for row in reader :
+                if matches := re.search( r"^(\d{4}),%s,(\d{1,3})$" %stdID , ','.join(row) , re.IGNORECASE ) :
+    #SEARCH function re.search takes a string and checks if the string is of exactly 4 characters if not it raises a Valueerror
+    #r it handles the '\' ( raw string literal) like a character not like "\ n".
+    # ^ indicates start of string.
+    #  \d{4} it means the string must contain 4 digits.
+    # "," A literal comma, matching exactly one comma in the input string.
+    # "$" asserts the end of the string.
+    # "%s" is a placeholder for the value of stdID. The % operator performs string formatting, substituting the value of stdID into the regular expression.
+    #This part will match the exact value of stdID in the input string. (replace value of stdID in the file)
+    #  \d{1,3} it means the string must contain 1 to 3 digits.
+    # re.IGNORECASE flag that makes the search insensitive
+                    stdRecord.append( [name , matches.group(1) , matches.group(2)] )
+
+    if len(stdRecord) :
+        print( tabulate( stdRecord , headers = [ "Course" , "Year" , "Grade" ] , tablefmt = "github" ) )
+    else :
+        print( "Student ID not found" )
+    
